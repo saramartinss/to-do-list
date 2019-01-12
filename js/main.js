7c579388e8b42cbf42d53a3f70d3e83b9851c5ff
sarinhamartins <sariinha.martins@gmail.com>
2019-01-12 21:45:06 -0200

    criando subtarefas

--- js/main.js
+++ js/main.js
@@ -1,20 +1,54 @@
 const toDoList = document.querySelector('.listas')
-const nodeListaBotaoAdicionarTarefa = document.querySelectorAll('.listas__botao-adicionar-tarefa')
-const nodeListInputAdicionarTarefa = document.querySelectorAll('.listas__input-adicionar-tarefa')
-const nodeListTarefas = document.querySelectorAll('.listas__item-tarefas')
 
-const botoes = Array.prototype.slice.call(nodeListaBotaoAdicionarTarefa)
-const input = Array.prototype.slice.call(nodeListInputAdicionarTarefa)
-const listas = Array.prototype.slice.call(nodeListTarefas)
+let inputAddSubtarefa = Array.prototype.slice.call(document.querySelectorAll('.listas__input-adicionar-subtarefa'))
+let botaoAddSubtarefa = Array.prototype.slice.call(document.querySelectorAll('.lista__botao-adicionar-subtarefa'))
 
-for(let i = 0; i < botoes.length; i++){
-    botoes[i].addEventListener('click', function(){
-        const lista = listas[i]
-        const titulo = input[i].value
-        criaTarefa(lista, titulo)
-    })
+function criaSubtarefa(lista, titulo){
+    const linha = document.createElement('li')
+    linha.classList.add('lista__subtarefa')
+    const subtarefa =
+    `<div>
+        <input type="checkbox" id="checkboxSubtarefa1" onclick="tarefaConcluida()">
+        <p>${titulo}</p>
+    </div>
+    <img class="lista__substarefa-deletar" src="../../assets/icone_deletar_tarefa-subtarefa.png"
+        alt="icone deletar substarefa">`
+    linha.innerHTML = subtarefa
+    lista.appendChild(linha)
 }
 
+function criaTarefa(lista, titulo){
+    const linha = document.createElement('li')
+    linha.classList.add('lista__tarefa')
+    const tarefa =
+    `
+    <div class="lista__tarefa-nome">
+        <div>
+            <input type="checkbox" id="checkboxTarefa1" onclick="tarefaConcluida()">
+            <p>${titulo}</p>
+        </div>
+        <img class="lista__tarefa-deletar" src="../../assets/icone_deletar_tarefa-subtarefa.png" alt="icone deletar tarefa">
+    </div>
+    <ul class="lista__subtarefas">
+    </ul>
+    <div class="lista__adicionar-subtarefa adicionar-item">
+        <label for="adicionarSubtarefa">
+                                            <input class="listas__input-adicionar-subtarefa" type="text" id="adicionarSubtarefa" placeholder="Adicionar subtarefa">
+                                        </label>
+                                        <img class="lista__botao-adicionar-subtarefa" src="../../assets/botao_adicionar.png"
+                                            alt="botão adicionar subtarefa">
+    </div>
+    `
+    linha.innerHTML = tarefa
+    lista.appendChild(linha)
+}
+
+function deletar(evento) {
+    evento.parentNode.classList.add('fadeOut')
+    setTimeout(function () {
+        evento.parentNode.remove()
+    }, 500)
+}
 
 toDoList.addEventListener('click', function (event) {
     if (event.target.tagName === 'IMG') {
@@ -24,33 +58,14 @@ toDoList.addEventListener('click', function (event) {
         if (event.target.classList.contains('lista__tarefa-deletar')) {
             deletar(event.target.parentNode)
         }
+        if (event.target.classList.contains('lista__botao-adicionar-subtarefa')){
+            const div = event.target.parentNode
+            const input = div.firstChild.nextSibling.firstChild.nextSibling
+            const titulo = input.value
+            const lista = div.parentNode.childNodes[3]
+            criaSubtarefa(lista, titulo)
+            input.value = ''
+        }
     }
 })
 
-function criaTarefa(lista, titulo){
-    const linha = document.createElement('li')
-        linha.classList.add('lista__tarefa')
-        const tarefa =
-        `<div class="lista__tarefa-nome">
-            <div>
-                <input type="checkbox" id="checkboxTarefa1" onclick="tarefaConcluida()">
-                <p>${titulo}</p>
-            </div>
-            <img class="lista__tarefa-deletar" src="../../assets/icone_deletar_tarefa-subtarefa.png" alt="icone deletar tarefa">
-        </div>
-        <div class="lista__adicionar-subtarefa adicionar-item">
-        <label for="adicionarSubtarefa">
-            <input type="text" id="adicionarSubtarefa" placeholder="Adicionar subtarefa">
-        </label>
-        <img class="lista__botao-adicionar-subtarefa" src="../../assets/botao_adicionar.png" alt="botão adicionar subtarefa">
-        </div>`
-        linha.innerHTML = tarefa
-        lista.appendChild(linha)
-}
-
-function deletar(evento) {
-    evento.parentNode.classList.add('fadeOut')
-    setTimeout(function () {
-        evento.parentNode.remove()
-    }, 500)
-}
