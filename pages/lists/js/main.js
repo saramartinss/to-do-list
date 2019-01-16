3197dda9bd2f7e4e0410b7386de157b8f96eb8e2
sarinhamartins <sariinha.martins@gmail.com>
2019-01-16 19:33:55 -0200

    validacao para que nao seja criada tarefa/subtarefa vazia

--- pages/lists/js/main.js
+++ pages/lists/js/main.js
@@ -11,6 +11,13 @@ toDoList.addEventListener('click', function (event) {
         if (event.target.classList.contains('list__btn-add-subtask')){
             const div = event.target.parentNode
             const input = div.firstChild.nextSibling.firstChild.nextSibling
+
+            const regex = /\w+/ig;
+            if(!regex.test(input.value)){
+                input.focus()
+                return false
+            }
+            
             const titulo = input.value
             const lista = div.parentNode.childNodes[3]
             criaSubtarefa(lista, titulo)
@@ -19,6 +26,13 @@ toDoList.addEventListener('click', function (event) {
         if (event.target.classList.contains('lists__btn-add-task')){
             const div = event.target.parentNode
             const input = div.firstChild.nextSibling.firstChild.nextSibling
+
+            const regex = /\w+/ig;
+            if(!regex.test(input.value)){
+                input.focus()
+                return false
+            }
+
             const titulo = input.value
             const lista = div.parentNode.childNodes[5]
             criaTarefa(lista, titulo)
