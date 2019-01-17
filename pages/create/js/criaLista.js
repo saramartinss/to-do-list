0f4f751db0fd11e8794693afb7aefda2e4121f02
sarinhamartins <sariinha.martins@gmail.com>
2019-01-17 12:54:58 -0200

    criacao de lista com todas tarefas

--- pages/create/js/criaLista.js
+++ pages/create/js/criaLista.js
@@ -17,16 +17,30 @@ btnCreateList.addEventListener('click', function(e){
 
     const li = document.createElement('li')
     li.classList.add('lists__item')
-    const div = `<div class="lists__item-title">
-    <div class="lists__item-name">
-        <img src="../../assets/icone_lista.png" alt="icone lista">
-        <p class="lists__list-name" ondblclick="trocaNome(this)">${inputListName.value}</p>
-    </div>
-    <div class="lists__item-icons">
-        <img src="../../assets/icone_editar.png" alt="icone deletar">
-        <img src="../../assets/icone_deletar_lista.png" alt="icone deletar">
-    </div>
-</div>`
+    const div = `
+        <div class="lists__item-title">
+            <div class="lists__item-name">
+            <img src="../../assets/icone_lista.png" alt="icone lista">
+            <p class="lists__list-name" ondblclick="trocaNome(this)">${inputListName.value}</p>
+            </div>
+            <div class="lists__item-icons">
+            <img src="../../assets/icone_editar.png" alt="icone deletar">
+            <img src="../../assets/icone_deletar_lista.png" alt="icone deletar">
+            </div>
+        </div>
+        <ul class="lists__item-tasks container-tasks"></ul>
+    `
     li.innerHTML = div
     listas.appendChild(li)
+    
+    const qtdTarefas = form.childElementCount
+    const listaDeTarefas = li.querySelector('.lists__item-tasks')
+
+    for(let i = 0; i < qtdTarefas; i++){
+        let input = form.children[i].querySelector('.create-list__input-task-name')
+        let tituloTarefa = input.value
+        criaTarefa(listaDeTarefas, tituloTarefa)
+        input.value = ''
+        inputListName.value = ''
+    }
 })
