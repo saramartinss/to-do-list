0bfe6099b4b0ca0a6b5260e725e9daaa01a9ed2c
sarinhamartins <sariinha.martins@gmail.com>
2019-01-28 18:21:36 -0200

    adicao de classe para fazer funcionar os botoes de deletar lista e editar

--- pages/create/js/criaLista.js
+++ pages/create/js/criaLista.js
@@ -21,11 +21,11 @@ btnCreateList.addEventListener('click', function(e){
         <div class="lists__item-title">
             <div class="lists__item-name">
             <img src="../../assets/icone_lista.png" alt="icone lista">
-            <p class="lists__list-name" ondblclick="trocaNome(this)">${inputListName.value}</p>
+            <p class="lists__list-name">${inputListName.value}</p>
             </div>
             <div class="lists__item-icons">
-            <img src="../../assets/icone_editar.png" alt="icone deletar">
-            <img src="../../assets/icone_deletar_lista.png" alt="icone deletar">
+            <img class="lists__edit-name" src="../../assets/icone_editar.png" alt="icone editar">
+            <img class="lists__delete-list" src="../../assets/icone_deletar_lista.png" alt="icone deletar">
             </div>
         </div>
         <ul class="lists__item-tasks container-tasks"></ul>
